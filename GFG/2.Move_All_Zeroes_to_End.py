022776fa22aa3a9630c3f60da50f1c735bcdcf9f
surajkr8428 <kr.suraj8428@gmail.com>
2025-10-14 23:42:26 +0530

    Code update

--- GFG/2.Move_All_Zeroes_to_End.py
+++ GFG/2.Move_All_Zeroes_to_End.py
@@ -1,14 +1,38 @@
 def PushZerosToEnd(arr):
     
-    z = arr.count(0)
-    for i in arr:
-        if i == 0:
-            arr.remove(i)
+    # z = arr.count(0)
+    # for i in arr:
+    #     if i == 0:
+    #         arr.remove(i)
     
-    for _ in range(z):
-        arr.append(0)
+    # for _ in range(z):
+    #     arr.append(0)
         
-    return arr
+    # return arr
+    
+    #User function Template for python3
+
+    # def pushZerosToEnd(self,arr):
+        # code here
+        pos = 0
+        
+        for i in range(len(arr)):
+            if arr[i] != 0:
+                arr[pos] = arr[i]
+                pos += 1
+                
+        while pos < len(arr):
+            arr[pos] = 0
+            pos += 1
+        
+        return arr
+        
+    #     print(*arr)
+        
+    #     return a
+    #     print("arr:",arr)
+        # return "".join(map(str,arr))
+
     
 ar = [[1, 2, 0, 4, 3, 0, 5, 0],[10, 20, 30],[0, 0]]
 for a in ar:
