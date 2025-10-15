fa8edd7cd66eaec0073112a8529267797da7d7cf
surajkr8428 <kr.suraj8428@gmail.com>
2025-10-15 16:49:42 +0530

    Code update

--- GFG/4.Rotate_Array.py
+++ GFG/4.Rotate_Array.py
@@ -1,25 +1,35 @@
 def rotateArr(arr, d):
     
-    n = len(arr)
-    # l = [0]*n
-    l = []
+    l = len(arr)
+    t = [0]*l
     
-    for i in range(d,n):
-        l.append(arr[i])
+    if d == l:
+        return arr
     
-    for j in range(d):
-        # l.append(arr[j])
-        pass
-    
-    for k in range(n):
-        arr[k] = l[k]
+    else:
+        if d > l:
+            d = d % l
+        
+        for i in range(l-d):
+            # t.append(arr[i])
+            t[i] = arr[d+i]
+        
+        for j in range(d):
+            t[l-d+j] = arr[j]
+            
+        for a in range(l):
+            arr[a] = t[a]
+        
+        return arr   
     
 ip1 = [[1, 2, 3, 4, 5],2]
 ip2 = [[2, 4, 6, 8, 10, 12, 14, 16, 18, 20],3]
 ip3 = [[7, 3, 9, 1],9]
 
+# print(rotateArr([1, 2, 3, 4, 5],2))
 ip = [ip1,ip2,ip3]
 
 for a in ip:
-    print(a[0],a[1])
+    # print(a[0],a[1])
+    # print(a[0],a[1])
     print(rotateArr(a[0],a[1]))
